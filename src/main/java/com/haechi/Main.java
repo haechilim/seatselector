72193db1a5d7d1db0f8d068b495eaf2be72436f1
Haechi <combuff@nethru.co.kr>
2019-07-20 10:16:46 +0900

    이성 후보가 다 소진된 경우 동성끼리 짝 맺기 로직 검사하는 함수 만들기

--- src/main/java/com/haechi/Main.java
+++ src/main/java/com/haechi/Main.java
@@ -5,101 +5,111 @@ public class Main {
     private static final int TOTAL_STUDENTS = 10;
 
     public static void main(String[] args) {
+        test1();
+        //run();
+    }
+
+    public static void test1() {
+        int round = 0;
         Student[] students = new Student[TOTAL_STUDENTS];
 
         for(int i = 0; i < TOTAL_STUDENTS; i++) {
             students[i] = new Student();
         }
 
-        {
-            students[0].setId(0);
-            students[1].setId(1);
-            students[2].setId(2);
-            students[3].setId(3);
-            students[4].setId(4);
-            students[5].setId(5);
-            students[6].setId(6);
-            students[7].setId(7);
-            students[8].setId(8);
-            students[9].setId(9);
-            /*students[10].setId(11);
-            students[11].setId(12);
-            students[12].setId(13);
-            students[13].setId(14);
-            students[14].setId(15);
-            students[15].setId(16);
-            students[16].setId(17);
-            students[17].setId(18);
-            students[18].setId(19);
-            students[19].setId(20);
-            students[20].setId(21);
-            students[21].setId(22);
-            students[22].setId(23);
-            students[23].setId(24);
-            students[24].setId(25);
-            students[25].setId(26);
-            students[26].setId(27);
-            students[27].setId(28);
-            students[28].setId(29);
-            students[29].setId(30);*/
+        students[0].setId(0);
+        students[1].setId(1);
+        students[2].setId(2);
+        students[3].setId(3);
+        students[4].setId(4);
+        students[5].setId(5);
+        students[6].setId(6);
+        students[7].setId(7);
+        students[8].setId(8);
+        students[9].setId(9);
 
-            students[0].setMale(false);
-            students[1].setMale(false);
-            students[2].setMale(false);
-            students[3].setMale(false);
-            students[4].setMale(false);
-            students[5].setMale(true);
-            students[6].setMale(false);
-            students[7].setMale(true);
-            students[8].setMale(false);
-            students[9].setMale(true);
-            /*students[10].setMale(false);
-            students[11].setMale(true);
-            students[12].setMale(false);
-            students[13].setMale(true);
-            students[14].setMale(false);
-            students[15].setMale(true);
-            students[16].setMale(false);
-            students[17].setMale(true);
-            students[18].setMale(false);
-            students[19].setMale(true);
-            students[20].setMale(false);
-            students[21].setMale(true);
-            students[22].setMale(false);
-            students[23].setMale(true);
-            students[24].setMale(false);
-            students[25].setMale(true);
-            students[26].setMale(false);
-            students[27].setMale(true);
-            students[28].setMale(false);
-            students[29].setMale(true);*/
+        students[0].setMale(false);
+        students[1].setMale(false);
+        students[2].setMale(false);
+        students[3].setMale(false);
+        students[4].setMale(false);
+        students[5].setMale(true);
+        students[6].setMale(false);
+        students[7].setMale(true);
+        students[8].setMale(false);
+        students[9].setMale(true);
+
+        run(round, students);
+        print(round, students);
+        if(students[0].getPartnerId(round) != 5) System.out.println("실패");
+        if(students[1].getPartnerId(round) != 7) System.out.println("실패");
+        if(students[2].getPartnerId(round) != 9) System.out.println("실패");
+        if(students[3].getPartnerId(round) != 4) System.out.println("실패");
+        if(students[4].getPartnerId(round) != 3) System.out.println("실패");
+        if(students[5].getPartnerId(round) != 0) System.out.println("실패");
+        if(students[6].getPartnerId(round) != 8) System.out.println("실패");
+        if(students[7].getPartnerId(round) != 1) System.out.println("실패");
+        if(students[8].getPartnerId(round) != 6) System.out.println("실패");
+        if(students[9].getPartnerId(round) != 2) System.out.println("실패");
+
+        run(++round, students);
+        print(round, students);
+        if(students[0].getPartnerId(round) != 7) System.out.println("실패");
+        if(students[1].getPartnerId(round) != 5) System.out.println("실패");
+        if(students[2].getPartnerId(round) != -1) System.out.println("실패");
+        if(students[3].getPartnerId(round) != 9) System.out.println("실패");
+        if(students[4].getPartnerId(round) != 6) System.out.println("실패");
+        if(students[5].getPartnerId(round) != 1) System.out.println("실패");
+        if(students[6].getPartnerId(round) != 4) System.out.println("실패");
+        if(students[7].getPartnerId(round) != 0) System.out.println("실패");
+        if(students[8].getPartnerId(round) != -1) System.out.println("실패");
+        if(students[9].getPartnerId(round) != 3) System.out.println("실패");
+
+        System.out.println("성공");
+    }
+
+    private static void run(int round, Student[] students) {
+        boolean check = true;
+        int maleCount = 0;
+        int femaleCount = 0;
+
+        init(students);
+
+        for(int i = 0; i < students.length; i++) {
+            if(students[i].getMale()) maleCount++;
+            else femaleCount++;
         }
 
-        for(int round = 0; round < TOTAL_ROUND; round++) {
-            init(students);
-            //shuffle(students);
+        for (int i = 0; i < students.length; i++) {
+            Student student = students[i];
 
-            for (int i = 0; i < students.length; i++) {
-                Student student = students[i];
+            if(maleCount == 0 || femaleCount == 0) check = false;
 
-                if (student.hasPartner()) continue;   // 짝이 결정된 학생인지 확인
+            if (student.hasPartner()) continue;   // 짝이 결정된 학생인지 확인
 
-                for (int j = i + 1; j < students.length; j++) {
-                    Student candidate = students[j];
+            for (int j = i + 1; j < students.length; j++) {
+                Student candidate = students[j];
 
-                    if (candidate.hasPartner()) continue;  // 짝이 결정된 학생인지 확인
-                    if (wasPartnerInThePast(student, candidate, round)) continue;   // 전에 짝이었는지 확인
-                    if (student.getMale() == candidate.getMale()) continue;   // 동성인지 확인
+                if (candidate.hasPartner()) continue;  // 짝이 결정된 학생인지 확인
+                if (wasPartnerInThePast(student, candidate, round)) continue;   // 전에 짝이었는지 확인
+                if (student.getMale() == candidate.getMale() && check) continue;   // 동성인지 확인
 
-                    student.setPartnerId(round, candidate.getId());
-                    candidate.setPartnerId(round, student.getId());
-                    student.setHasPartner(true);
-                    candidate.setHasPartner(true);     // 두학생의 정보 갱신(짝이 됨)
-                    break;
+                student.setPartnerId(round, candidate.getId());
+                candidate.setPartnerId(round, student.getId());
+                student.setHasPartner(true);
+                candidate.setHasPartner(true);     // 두학생의 정보 갱신(짝이 됨)
+
+                if(check) {
+                    maleCount--;
+                    femaleCount--;
                 }
-            }
 
-            print(students, round);
+                /*System.out.println(maleCount);
+                System.out.println(femaleCount);
+                System.out.println();
+                System.out.println();*/
+                break;
+            }
         }
     }
 
@@ -130,12 +140,76 @@ public class Main {
         return false;
     }
 
-    private static void print(Student[] students, int theNumberOfTimes) {
+    private static void print(int round, Student[] students) {
         for(int i = 0; i < students.length; i++) {
-            System.out.println(students[i].getId() + "  " + students[i].getMale() + "  " + students[i].getPartnerId(theNumberOfTimes));
+            System.out.println(students[i].getId() + "  " + students[i].getMale() + "  " + students[i].getPartnerId(round));
         }
 
         System.out.println();
         System.out.println();
     }
+
+
+    /*
+    students[0].setId(0);
+    students[1].setId(1);
+    students[2].setId(2);
+    students[3].setId(3);
+    students[4].setId(4);
+    students[5].setId(5);
+    students[6].setId(6);
+    students[7].setId(7);
+    students[8].setId(8);
+    students[9].setId(9);
+    students[10].setId(11);
+    students[11].setId(12);
+    students[12].setId(13);
+    students[13].setId(14);
+    students[14].setId(15);
+    students[15].setId(16);
+    students[16].setId(17);
+    students[17].setId(18);
+    students[18].setId(19);
+    students[19].setId(20);
+    students[20].setId(21);
+    students[21].setId(22);
+    students[22].setId(23);
+    students[23].setId(24);
+    students[24].setId(25);
+    students[25].setId(26);
+    students[26].setId(27);
+    students[27].setId(28);
+    students[28].setId(29);
+    students[29].setId(30);
+
+    students[0].setMale(false);
+    students[1].setMale(false);
+    students[2].setMale(false);
+    students[3].setMale(false);
+    students[4].setMale(false);
+    students[5].setMale(true);
+    students[6].setMale(false);
+    students[7].setMale(true);
+    students[8].setMale(false);
+    students[9].setMale(true);
+    students[10].setMale(false);
+    students[11].setMale(true);
+    students[12].setMale(false);
+    students[13].setMale(true);
+    students[14].setMale(false);
+    students[15].setMale(true);
+    students[16].setMale(false);
+    students[17].setMale(true);
+    students[18].setMale(false);
+    students[19].setMale(true);
+    students[20].setMale(false);
+    students[21].setMale(true);
+    students[22].setMale(false);
+    students[23].setMale(true);
+    students[24].setMale(false);
+    students[25].setMale(true);
+    students[26].setMale(false);
+    students[27].setMale(true);
+    students[28].setMale(false);
+    students[29].setMale(true);*/
 }
